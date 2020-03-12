e811b87d5401a25d64efe1f04aa60a55061a8eed
Aina <ainaard@mail.ru>
2020-03-12 21:47:49 +0300

    first commit

---
+++ src/main/java/ru/itis/dogger/DoggerApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.dogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoggerApplication.class, args);
+    }
+
+}
