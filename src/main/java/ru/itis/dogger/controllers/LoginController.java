381e145c96cf8e4ed3f2524584a7f99fed15307e
Aina <ainaard@mail.ru>
2020-04-04 16:03:57 +0500

    ***

--- src/main/java/ru/itis/dogger/controllers/LoginController.java
+++ src/main/java/ru/itis/dogger/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package ru.itis.dogger.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,9 +21,25 @@ public class LoginController {
         this.usersService = usersService;
     }
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    void sendEmail() {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("ainaard@mail.ru");
+
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("Hello World \n Spring Boot Email");
+
+        javaMailSender.send(msg);
+
+    }
+
     @PostMapping("/login")
     @PreAuthorize("permitAll()")
     public TokenDto login(@RequestBody OwnerDto dto) {
+        sendEmail();
         return usersService.login(dto);
     }
 }
