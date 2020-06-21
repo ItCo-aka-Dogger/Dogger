5ba351c03a8bd14ac42c9bc0af2d0a94b39ad771
Aina <ainaard@mail.ru>
2020-06-22 00:46:48 +0500

    response entity fix

--- src/main/java/ru/itis/dogger/controllers/LoginController.java
+++ src/main/java/ru/itis/dogger/controllers/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
         if (token.getStatus() == TokenStatus.INVALID) {
             return new ResponseEntity<>(token.getValue(), HttpStatus.BAD_REQUEST);
         } else {
-            return ResponseEntity.ok(token);
+            return ResponseEntity.ok(token.getValue());
         }
     }
 }
