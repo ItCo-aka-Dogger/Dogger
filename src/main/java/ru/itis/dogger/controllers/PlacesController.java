e1f445bd14a4f663f28722101b55c671b0362373
Kamila Nigmetzianova <kamila-nigmet@mail.ru>
2020-06-24 17:49:13 +0300

    updated api for places

--- src/main/java/ru/itis/dogger/controllers/PlacesController.java
+++ src/main/java/ru/itis/dogger/controllers/PlacesController.java
@@ -52,7 +52,7 @@ public class PlacesController {
         if (place.isPresent()) {
             return ResponseEntity.ok(place);
         } else
-            return ResponseEntity.notFound().build();
+            return new ResponseEntity<>("There is no place with such id", HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping("/places/{placeId}/addComment")
