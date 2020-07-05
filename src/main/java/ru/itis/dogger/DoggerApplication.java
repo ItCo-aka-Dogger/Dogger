b77f012dd1fb3177dafe1ba82bbc9e78e355809f
Artur Badretdinov <artursletter@gmail.com>
2020-07-05 11:43:11 +0300

    Add Mongo dependency

--- src/main/java/ru/itis/dogger/DoggerApplication.java
+++ src/main/java/ru/itis/dogger/DoggerApplication.java
@@ -3,12 +3,14 @@ package ru.itis.dogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import ru.itis.dogger.services.EmailService;
 import ru.itis.dogger.services.EmailServiceImpl;
 
 import java.util.UUID;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class DoggerApplication {
 
     public static void main(String[] args) {
